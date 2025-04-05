ca8d755853b2686c1a9f6f990e4b4bd74425d6f4
Tony F. <a20223209@pucp.edu.pe>
2025-04-04 20:57:12 -0500

    Add files via upload
    Primer commit

---
+++ LAB1_GTICS_20251_20223209/src/test/java/org/example/lab1_gtics_20251_20223209/Lab1Gtics2025120223209ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lab1_gtics_20251_20223209;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1Gtics2025120223209ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
